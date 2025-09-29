bfca6c6b072c90a19b797ab70ee7ccbd2ce7a44b
Denzel <elliothdenzel14@outlook.com>
2025-09-29 15:24:12 -0600

    basic lobby system implemented

--- .github/RockyCatsGame/Assets/Scripts/Launcher.cs
+++ .github/RockyCatsGame/Assets/Scripts/Launcher.cs
@@ -60,6 +60,11 @@ public class Launcher : MonoBehaviourPunCallbacks
 
         Player[] players = PhotonNetwork.PlayerList;
 
+        foreach(Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
         for (int i = 0; i < players.Count(); i++)
         {
             Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
@@ -110,6 +115,8 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
         for (int i = 0; i < roomList.Count; i++)
         {
+            if(roomList[i].RemovedFromList)
+                continue;
             Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
         }
     }
