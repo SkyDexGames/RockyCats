f8d4015ad159a33d453f1b465682c866da96e88d
Denzel <elliothdenzel14@outlook.com>
2025-10-09 22:55:40 -0600

    AHORA SI FIX FINAL FINAL FINAL

--- RockyCatsGame/Assets/Scripts/IgneousBreakableWall.cs
+++ RockyCatsGame/Assets/Scripts/IgneousBreakableWall.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class IgneousBreakableWall : MonoBehaviour, IIgneousInteractable
 {
-    //aqui pones lo que quieras que haga cuando la ignea haga el dash y colisione con el objeto
+    private PhotonView photonView;
+
+    void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
     public void OnIgneousCollision()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            photonView.RPC("RPC_BreakWall", RpcTarget.AllBuffered);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    [PunRPC]
+    void RPC_BreakWall()
     {
         Destroy(gameObject);
     }
