3c601dc98493cdc5f5e651cca8988a0849061821
JoseGlezMtz <A01028517@tec.mx>
2025-10-12 18:29:20 -0600

    Encryption of messages

--- RockyCatsGame/Assets/Scripts/Messaging/TextChatManager.cs
+++ RockyCatsGame/Assets/Scripts/Messaging/TextChatManager.cs
@@ -1,65 +1,153 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
 
-public class TextChatManager : MonoBehaviour
+public class TextChatManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
+    
     public int maxMessages = 25;
-
-    public GameObject chatPanel, textObject;
+    public GameObject chatPanel;
+    public GameObject textPrefab;
     public TMP_InputField chatBox;
 
-    [SerializeField]
-    List<Message> messageList = new List<Message>();
+    private List<Message> messageList = new List<Message>();
+    private const byte CHAT_EVENT = 10;
+
+    private bool aesReady = false;
 
     void Start()
     {
+        PhotonNetwork.AddCallbackTarget(this);
 
+        if (PhotonNetwork.IsMasterClient)
+        {
+            AES256.GenerateNewKey();
+
+            var props = new ExitGames.Client.Photon.Hashtable
+            {
+                { "aesKey", AES256.GetKeyBase64() },
+                { "aesIV", AES256.GetIVBase64() }
+            };
+
+            PhotonNetwork.CurrentRoom.SetCustomProperties(props);
+            aesReady = true;
+            Debug.Log("MasterClient generated AES-256 key.");
+        }
+        else
+        {
+            TryLoadAesKey();
+        }
     }
-    
+
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        TryLoadAesKey();
+    }
+
+    private void TryLoadAesKey()
+    {
+        if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("aesKey", out object k) &&
+            PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue("aesIV", out object v))
+        {
+            AES256.SetKey((string)k, (string)v);
+            aesReady = true;
+            Debug.Log(" AES-256 key loaded for chat encryption.");
+        }
+    }
+
+    void OnDestroy()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
     void Update()
     {
-        if(chatBox.text != "")
+        // Permitir enviar con Enter
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            if(Input.GetKeyDown(KeyCode.Return))
+            if (chatBox.isFocused && !string.IsNullOrWhiteSpace(chatBox.text))
             {
-                SendMessageToChat(chatBox.text);
+                OnSendMessage();
                 chatBox.text = "";
                 chatBox.ActivateInputField();
             }
-        }
-        else if(!chatBox.isFocused)
-        {
-            if(Input.GetKeyDown(KeyCode.Return))
+            else
             {
                 chatBox.ActivateInputField();
             }
         }
-        
+    }
+
+    public void OnSendMessage()
+    {
+        if (!aesReady)
+        {
+            Debug.LogWarning("AES not ready yet, message not sent.");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(chatBox.text))
+            return;
+
+        string playerName = PhotonNetwork.NickName;
+        string message = $"{playerName}: {chatBox.text}";
+        string encrypted = AES256.Encrypt(message);
+        Debug.Log($"Sending encrypted message: {encrypted}");
+
+        PhotonNetwork.RaiseEvent(
+            CHAT_EVENT,
+            encrypted,
+            new RaiseEventOptions { Receivers = ReceiverGroup.All },
+            SendOptions.SendReliable
+        );
+
+        chatBox.text = "";
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == CHAT_EVENT)
+        {
+            string encryptedMsg = (string)photonEvent.CustomData;
+
+            if (!aesReady)
+            {
+                Debug.LogWarning("AES key not ready, cannot decrypt yet.");
+                return;
+            }
+
+            try
+            {
+                string decryptedMsg = AES256.Decrypt(encryptedMsg);
+                Debug.Log($"Received decrypted message: {decryptedMsg}");
+                SendMessageToChat(decryptedMsg);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"Decryption failed: {e.Message}");
+            }
+        }
     }
 
     public void SendMessageToChat(string text)
     {
         if (string.IsNullOrEmpty(text)) return;
 
-        if(messageList.Count >= maxMessages)
+        if (messageList.Count >= maxMessages)
         {
-            if (messageList[0].textObject != null)
-                Destroy(messageList[0].textObject.gameObject);
+            Destroy(messageList[0].textObject.gameObject);
             messageList.RemoveAt(0);
         }
 
-        Message newMessage = new Message();
-        newMessage.text = text;
+        GameObject newTextObj = Instantiate(textPrefab, chatPanel.transform);
+        TMP_Text tmpText = newTextObj.GetComponent<TMP_Text>();
+        tmpText.text = text;
 
-        GameObject newText = Instantiate(textObject, chatPanel.transform);
-        
-        newMessage.textObject = newText.GetComponent<TMP_Text>();
-
-        newMessage.textObject.text = newMessage.text;
-        messageList.Add(newMessage);
+        messageList.Add(new Message { text = text, textObject = tmpText });
     }
 
     [System.Serializable]
@@ -67,6 +155,5 @@ public class TextChatManager : MonoBehaviour
     {
         public string text;
         public TMP_Text textObject;
-
     }
 }
